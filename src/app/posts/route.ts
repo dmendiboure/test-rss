187455c88f8b831ae20bbe12ddc32f9b303e4441
Dylan Mendiboure <dmendiboure@isdecisions.com>
2025-07-18 15:47:31 +0200

    temp

--- src/app/posts/route.ts
+++ src/app/posts/route.ts
@@ -1,8 +1,18 @@
 export const revalidate = 60;
 
 export async function GET() {
-  const data = await fetch("https://api.vercel.app/blog");
-  const posts = await data.json();
+  //   const data = await fetch("https://api.vercel.app/blog");
+  //   const posts = await data.json();
 
-  return Response.json(posts);
+  return new Response(
+    JSON.stringify({
+      message: "Hello from the posts route!",
+      // posts: posts,
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
