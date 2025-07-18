b8e20543c00c3c146edd1f60480b57759d61c573
Dylan Mendiboure <dmendiboure@isdecisions.com>
2025-07-18 15:51:20 +0200

    temp

--- src/app/[locale]/posts/route.ts
+++ src/app/[locale]/posts/route.ts
@@ -1,7 +1,7 @@
 export const revalidate = 60;
+export const dynamic = "force-static";
 
 export async function GET() {
-  // rss feed
   const feed =
     "<rss version='2.0'><channel><title>My RSS Feed</title><link>http://example.com</link><description>This is a sample RSS feed</description></channel></rss>";
 
